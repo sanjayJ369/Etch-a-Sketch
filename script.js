294fa1eaacaf6f734e9af738d96351d8b7d492a3
cosmos-369 <cosmos333999@gmail.com>
2023-07-26 20:45:04 +0530

    add sketching and ability to change pixels feature
    added sketching feature and the ability to change the
    number of pixels on the grid,

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sketchPad = document.querySelector(".sketchPad");
 console.log(sketchPad.offsetWidth);
 
-const pixelCount = 30;
+const pixelCount = 24;
 const pixelSize = (sketchPad.offsetWidth)/ pixelCount;
 
 //sets the pixel size of each pixel
@@ -18,5 +18,37 @@ for (let i=0; i<pixelCount; i++) {
         pixels.push(pixel);
     }
 }
+
+//code to check if the mouse button is click or not
+let mouseDown = false;
+document.body.onmousedown = function () {
+    mouseDown = true;
+};
+
+
+document.body.onmouseup = function () {
+    mouseDown = false;
+};
+
+
+//function which return the color to be set
+function getColor() {
+    return "black";
+}
+
+//function to check the background of the pixels
+function colorPixel(e) {
+    if (mouseDown) e.target.style.cssText = "background-color: black";
+}
+
+//adding event listeners to the pixels
+
+pixels.forEach (pixel => {
+    pixel.addEventListener("mousedown", (e) => {
+        console.log("hi");
+        e.target.style.cssText = `background-color: ${getColor()}`;
+    });
+    pixel.addEventListener("mouseover", colorPixel);
+});
 
 console.log(pixels.length);

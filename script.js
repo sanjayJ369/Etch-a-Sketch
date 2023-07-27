79b32c56349a75609b8e80e4bd131e1149a463e0
cosmos-369 <cosmos333999@gmail.com>
2023-07-27 21:51:42 +0530

    add basic drawing function
    added basic drawing fucntions, clear, erase, rainbow,
    and color select

--- script.js
+++ script.js
@@ -1,54 +1,134 @@
-const sketchPad = document.querySelector(".sketchPad");
-console.log(sketchPad.offsetWidth);
 
-const pixelCount = 24;
-const pixelSize = (sketchPad.offsetWidth)/ pixelCount;
+let pixelCount = 10;
+let colorMode = "black";
+const shade = document.querySelector("#shade");
 
-//sets the pixel size of each pixel
-document.documentElement.style.setProperty("--pixelSize", `${pixelSize}px`)
 
-let pixels = [];
+//function to convert hex into rgba
+function hexToRGB(hex, alpha) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
 
-for (let i=0; i<pixelCount; i++) {
-    for (let j=0; j<pixelCount; j++) {
-        let pixel = document.createElement('div');
-        pixel.setAttribute('class', 'pixel');
-
-        sketchPad.appendChild(pixel);
-        pixels.push(pixel);
+    if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
     }
 }
 
-//code to check if the mouse button is click or not
-let mouseDown = false;
-document.body.onmousedown = function () {
-    mouseDown = true;
-};
-
-
-document.body.onmouseup = function () {
-    mouseDown = false;
-};
-
 
 //function which return the color to be set
+function getRandomColor() {
+    let r = Math.random() * 255;
+    let g = Math.random() * 255;
+    let b = Math.random() * 255;
+    let a = 1;
+    
+    if (shade.checked) {
+        a = 0.1;
+    }
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
 function getColor() {
-    return "black";
+    if (colorMode == "erase") {
+        return "white";
+    } else if (colorMode == "rainbow") {
+        return getRandomColor();
+    }
+    if (shade.checked) {
+        return hexToRGB(document.querySelector("#colorPicker").value, 0.1);
+    }
+    return document.querySelector("#colorPicker").value;
 }
 
-//function to check the background of the pixels
-function colorPixel(e) {
-    if (mouseDown) e.target.style.cssText = "background-color: black";
-}
 
-//adding event listeners to the pixels
+function createSketchpad(p) {
 
-pixels.forEach (pixel => {
-    pixel.addEventListener("mousedown", (e) => {
-        console.log("hi");
-        e.target.style.cssText = `background-color: ${getColor()}`;
+    const sketchPad = document.querySelector(".sketchPad");
+    //delete previosly added pixels to the sketchpad
+    document.querySelectorAll(".pixel").forEach((p) => {
+        sketchPad.removeChild(p);
     });
-    pixel.addEventListener("mouseover", colorPixel);
+
+
+    const pixelCount = p;
+    const pixelSize = (sketchPad.offsetWidth)/ pixelCount;
+
+    //sets the pixel size of each pixel
+    document.documentElement.style.setProperty("--pixelSize", `${pixelSize}px`)
+
+    let pixels = [];
+
+    for (let i=0; i<pixelCount; i++) {
+        for (let j=0; j<pixelCount; j++) {
+            let pixel = document.createElement('div');
+            pixel.setAttribute('class', 'pixel');
+
+            sketchPad.appendChild(pixel);
+            pixels.push(pixel);
+        }
+    }
+
+    //code to check if the mouse button is click or not
+    let mouseDown = false;
+    document.body.onmousedown = function () {
+        mouseDown = true;
+    };
+
+
+    document.body.onmouseup = function () {
+        mouseDown = false;
+    };
+
+    //function to set the background of the pixels
+    function colorPixel(e) {
+        if (mouseDown) e.target.style.cssText = `background-color: ${getColor()}`;
+    }
+
+    //adding event listeners to the pixels
+
+    pixels.forEach (pixel => {
+        pixel.addEventListener("mousedown", (e) => {
+            e.target.style.cssText = `background-color: ${getColor()}`;
+        });
+        pixel.addEventListener("mouseover", colorPixel);
+    });
+}
+
+
+//setups the inital sketchpad
+createSketchpad(pixelCount);
+
+
+//clears the sketchpad if the clear button is pressed 
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", (e) => {
+    colorMode = "black";
+    createSketchpad(pixelCount);
 });
 
-console.log(pixels.length);
+
+//functions to change the color of pen when rainbow, erase buttons 
+//are pressed 
+const colorModes = document.querySelectorAll("*[data-color]");
+console.log(colorModes);
+colorModes.forEach((mode) => {
+    mode.addEventListener("click", (e) => {
+        colorMode = (e.target.dataset.color);
+    })
+});
+
+
+//variable that toggles between true and false
+//const colorShade = document.querySelector("#shade");
+//colorShade.addEventListener("click", (e) => {
+//    shade = shade ? false : true;
+//});
+
+const colorPicker = document.querySelector("#colorPicker");
+colorPicker.addEventListener("input", (e) => {
+    colorMode = "none";
+});
+

ba2cae9bd937ca973fd0cc198ec6063db8fd4788
cosmos-369 <cosmos333999@gmail.com>
2023-07-28 21:02:04 +0530

    add shade feature

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const shade = document.querySelector("#shade");
 
 //function to convert hex into rgba
 function hexToRGB(hex, alpha) {
-    var r = parseInt(hex.slice(1, 3), 16),
+    let r = parseInt(hex.slice(1, 3), 16),
         g = parseInt(hex.slice(3, 5), 16),
         b = parseInt(hex.slice(5, 7), 16);
 
@@ -23,23 +23,69 @@ function getRandomColor() {
     let r = Math.random() * 255;
     let g = Math.random() * 255;
     let b = Math.random() * 255;
-    let a = 1;
     
-    if (shade.checked) {
-        a = 0.1;
-    }
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
-function getColor() {
+//function that convertes rgb or rgba 
+//into rgba and adds some alpha to it
+function addAlpha(rgb, alpha=0.1) {
+
+    if (rgb.slice(0, 4) == "rgb(") {
+        let rgba = rgb.replace("rgb", "rgba")
+        return (rgba.replace(")", `, ${alpha})`));
+    } else {
+        let rgba = rgb.split(",");
+        let r = parseInt(rgba[0].replace("rgba(", "")),
+        g = parseInt(rgba[1]),
+        b = parseInt(rgba[2]),
+        a = parseFloat(rgba[3].replace(")", ""));
+        
+        a += alpha;
+        if (a >= 1) a = 0.9;
+
+        console.log(r, g, b, a);
+
+        return `rgba(${r}, ${g}, ${b}, ${a})`;
+    }
+}
+
+function getColor(p) {
+
     if (colorMode == "erase") {
         return "white";
+    } else if (shade.checked) {
+
+        /*
+            if the pixel does not have any background color i.e it's 
+            color is white add shade of the color present in the 
+            color picker
+
+            if the colorMode is rainbow and it does not have any background 
+            color get random color and add alpha to it
+
+
+        */
+        if (colorMode == "rainbow" && !p.style.backgroundColor) {
+            let randColor = getRandomColor();
+            console.log(addAlpha(randColor));
+            return addAlpha(randColor);
+        } 
+
+        if (!p.style.backgroundColor) {
+            return (addAlpha(hexToRGB(document.querySelector("#colorPicker").value)));
+        }
+        
+        if (p.style.backgroundColor) {
+            if (p.style.backgroundColor.slice(0, 4) == "rgba") {
+                return addAlpha(p.style.backgroundColor);
+            }
+        }
+        //return (addAlpha(hexToRGB(document.querySelector("#colorPicker").value)));
     } else if (colorMode == "rainbow") {
         return getRandomColor();
     }
-    if (shade.checked) {
-        return hexToRGB(document.querySelector("#colorPicker").value, 0.1);
-    }
+    
     return document.querySelector("#colorPicker").value;
 }
 
@@ -84,14 +130,14 @@ function createSketchpad(p) {
 
     //function to set the background of the pixels
     function colorPixel(e) {
-        if (mouseDown) e.target.style.cssText = `background-color: ${getColor()}`;
+        if (mouseDown) e.target.style.cssText = `background-color: ${getColor(e.target)}`;
     }
 
     //adding event listeners to the pixels
 
     pixels.forEach (pixel => {
         pixel.addEventListener("mousedown", (e) => {
-            e.target.style.cssText = `background-color: ${getColor()}`;
+            e.target.style.cssText = `background-color: ${getColor(e.target)}`;
         });
         pixel.addEventListener("mouseover", colorPixel);
     });
@@ -121,14 +167,12 @@ colorModes.forEach((mode) => {
 });
 
 
-//variable that toggles between true and false
-//const colorShade = document.querySelector("#shade");
-//colorShade.addEventListener("click", (e) => {
-//    shade = shade ? false : true;
-//});
 
 const colorPicker = document.querySelector("#colorPicker");
 colorPicker.addEventListener("input", (e) => {
     colorMode = "none";
 });
+colorPicker.addEventListener("click", (e) => {
+    colorMode = "none";
+})
 
